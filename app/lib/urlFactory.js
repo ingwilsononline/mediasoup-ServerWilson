8f9559cae220b8d0bc6c61287274c35bf8637ba7
WCNosliwsys <nosliwsys@gmail.com>
2022-11-08 23:38:33 -0500

    prueba de cambio de wss para administrarlo con nginx

--- app/lib/urlFactory.js
+++ app/lib/urlFactory.js
@@ -7,5 +7,6 @@ export function getProtooUrl({ roomId, peerId })
 {
 	const hostname = window.location.hostname;
 
-	return `wss://${hostname}:${protooPort}/?roomId=${roomId}&peerId=${peerId}`;
+	//return `wss://${hostname}:${protooPort}/?roomId=${roomId}&peerId=${peerId}`;
+	return `wss://${hostname}/wss/?roomId=${roomId}&peerId=${peerId}`;
 }
